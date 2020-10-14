7493dd401bed20d6259aa10350e44cc4be0d5ebf
Stefan Schneider <stefan.schneider@upb.de>
2020-10-14 19:31:52 +0200

    order previously tracked entries by date

--- app/views.py
+++ app/views.py
@@ -1,3 +1,5 @@
+import datetime
+
 from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
 from django.template import loader
@@ -8,10 +10,33 @@ from .models import Entry
 from .forms import EntryForm
 
 
+def order_entries(entries, last_days=7):
+    """
+    Order the given entries by date and return a dict that has a key for the last last_days days.
+    Each key points to a (possibly empty) list of entries of that day
+    """
+    today = datetime.date.today()
+    days = [today - datetime.timedelta(days=i) for i in range(last_days)]
+    # construct dict with empty list for each day
+    entry_dict = {day: [] for day in days}
+    # fill it by iterating over the list of given entries
+    for e in entries:
+        if e.date in entry_dict:
+            entry_dict[e.date].append(e)
+    return entry_dict
+
+
 @login_required
 def index(request):
     entries = Entry.objects.filter(owner=request.user).order_by('-date', 'name')
-    tracked_dates = Entry.objects.values('date').distinct()
+    # tracked_dates = Entry.objects.values('date').distinct()
+    # construct dict with date --> list of entries; ordered by default in Python 3.6+
+    entry_dict = dict()
+    for e in entries:
+        if e.date in entry_dict:
+            entry_dict[e.date].append(e)
+        else:
+            entry_dict[e.date] = [e]
 
     # if this is a POST request we need to process the form data
     if request.method == 'POST':
@@ -29,8 +54,9 @@ def index(request):
 
     # prepare context
     context = {
-        'entries': entries,
-        'dates': tracked_dates,
+        # 'entries': entries,
+        'entry_dict': entry_dict,
+        # 'dates': tracked_dates,
         'form': form,
     }
 
