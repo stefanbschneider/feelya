9f39efca73c7dbf5fc9cb26402da42624a0baa7e
Stefan Schneider <stefan.schneider@upb.de>
2020-10-14 20:09:34 +0200

    add dependency

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ requirements = [
     'django-registration>=3.1.1',
     'gunicorn>=20.0.4',
     'dj-database-url>=0.5.0',
-    # 'psycopg2-binary>=2.8.5',
+    'psycopg2-binary>=2.8.6',
     # 'freezegun',
     'django-pwa>=1.0.10',
     'whitenoise>=5.2.0',
