cff1de9873f12f63e24d4450d804dc0d01a50f16
Stefan Schneider <stefan.schneider@upb.de>
2020-10-14 20:05:36 +0200

    production settings

--- README.md
+++ README.md
@@ -1,2 +1,29 @@
 # feelya
+
 ✔️😊 feelya: The app that gets you! Keep track of what you eat and do and improve how you feel. 
+
+## Deployment
+
+### Local
+
+```
+# serve
+python manage.py runserver
+
+# test
+python manage.py test app
+```
+
+### Production Deployment on Heroku
+
+Deployment is automatically updated with new pushes to `master`.
+
+Set the following config vars in Heroku (= env vars):
+
+* `DJANGO_SETTINGS_MODULE`: `project.prod_settings`
+* `DJANGO_SECRET_KEY`: `<randomly-generated-secret-key>`
+* `DATABASE_URL`: URL to Heroku Postgres DB
+* `SENDGRID_API_KEY`: `<sendgrid-api-key>`
+
+For serving static files (e.g., favicon) in production, Ideally uses `whitenoise`.
+For deployment, also `Procfile` and `runtime.txt` are relevant.
---
+++ project/prod_settings.py
@@ -0,0 +1,29 @@
+""" Production Settings """
+import dj_database_url
+
+# default: use settings from main settings.py if not overwritten
+from .settings import *
+
+
+############
+# SECURITY #
+############
+
+DEBUG = False
+
+DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
+
+SECRET_KEY = os.getenv('DJANGO_SECRET_KEY', SECRET_KEY)
+
+ALLOWED_HOSTS = ['feelya-app.herokuapp.com']
+
+# production email backend using sendgrid
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.sendgrid.net'
+EMAIL_PORT = 465
+EMAIL_HOST_USER = 'apikey'
+EMAIL_HOST_PASSWORD = os.getenv('SENDGRID_API_KEY')
+EMAIL_USE_SSL = True
+
+# disable console.log debugging for django-pwa
+# PWA_APP_DEBUG_MODE = False
